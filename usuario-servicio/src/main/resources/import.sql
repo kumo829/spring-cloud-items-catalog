aa9cab73a0f9550499072831a43c6a7ce88c504f
kumo829 <amonmar2000@hotmail.com>
2020-03-10 21:16:49 -0600

    Added PostgreSQL support to users service

--- usuario-servicio/src/main/resources/import.sql
+++ usuario-servicio/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres', '$2a$10$ITzydHqGvVtZB5YTmlC0P.OfnlWHZK1GcDGq7.Yy1zgv4QLNYunXu', 1, 'Andres', 'Guzman', 'profesor@javatutoriales.com')
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$thAYFr3OLNbQwHHHdFVFOuGu/PXZ//1XGlQ2nfk2oiwXIH97wSuD6', 1, 'Admin', 'Sitio', 'admin@javatutoriales.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres', '$2a$10$ITzydHqGvVtZB5YTmlC0P.OfnlWHZK1GcDGq7.Yy1zgv4QLNYunXu', true, 'Andres', 'Guzman', 'profesor@javatutoriales.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$thAYFr3OLNbQwHHHdFVFOuGu/PXZ//1XGlQ2nfk2oiwXIH97wSuD6', true, 'Admin', 'Sitio', 'admin@javatutoriales.com')
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
